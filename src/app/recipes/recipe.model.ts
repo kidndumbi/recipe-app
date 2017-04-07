5d772d47884cdf9393d807e8e829327cb877ecf0
Cassava <kidndumbi@gmail.com>
2017-04-06 22:29:12 -0400

    created ui and added recipe model ts file

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,3 +1,15 @@
 /**
  * Created by Cassava on 4/6/2017.
  */
+export class Recipe {
+  public name: string;
+  public description: string;
+  public imagePath: string;
+
+  constructor(name: string, description: string, imagePath: string) {
+     this.name = name;
+     this.description = description;
+     this.imagePath = imagePath;
+  }
+
+}
