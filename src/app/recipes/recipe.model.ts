ba1bda947d3cea8b8a23bea39930f83e8747e507
Cassava <kidndumbi@gmail.com>
2017-04-17 04:03:40 -0400

    recipe and shopping-list services

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,3 +1,4 @@
+import {Ingredient} from '../shared/ingredient.model';
 /**
  * Created by Cassava on 4/6/2017.
  */
@@ -5,11 +6,13 @@ export class Recipe {
   public name: string;
   public description: string;
   public imagePath: string;
+  public ingredients: Ingredient[];
 
-  constructor(name: string, description: string, imagePath: string) {
+  constructor(name: string, description: string, imagePath: string, ingredienst: Ingredient[]) {
      this.name = name;
      this.description = description;
      this.imagePath = imagePath;
+     this.ingredients = ingredienst;
   }
 
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import {Recipe} from './recipe.model';
+import {Ingredient} from '../shared/ingredient.model';
 
 @Injectable()
 export class RecipeService {
@@ -8,11 +9,32 @@ export class RecipeService {
 
 
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2016/01/08/08/31/paella-1127334_960_720.jpg'),
-    new Recipe('Second recipe', 'This is simply a test', 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTbnMvC3Hsa7hCo4LlizmxCwqlaYKUadOEskWuXEUpG8yhvVa8E'),
-    new Recipe('Third recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a8/Chicken-kathi-roll-recipe.jpg/640px-Chicken-kathi-roll-recipe.jpg'),
-    new Recipe('fourth recipe', 'This is simply a test', 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQRpGdr_SnetaYnH4mKII5dqx9lDI6_juo2QPl0rvztlSlfIUg'),
-    new Recipe('fifth recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/d/df/Dahi_baigana_Odia_recipe.jpg'),
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2016/01/08/08/31/paella-1127334_960_720.jpg', [
+      new Ingredient('meat', 1),
+      new Ingredient('paw paw', 2),
+      new Ingredient('pork', 3)
+
+    ]),
+    new Recipe('Second recipe', 'This is simply a test', 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTbnMvC3Hsa7hCo4LlizmxCwqlaYKUadOEskWuXEUpG8yhvVa8E', [
+      new Ingredient('peanut', 1),
+      new Ingredient('orange', 2),
+      new Ingredient('pork', 3)
+    ]),
+    new Recipe('Third recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a8/Chicken-kathi-roll-recipe.jpg/640px-Chicken-kathi-roll-recipe.jpg', [
+      new Ingredient('Berries', 1),
+      new Ingredient('paw paw', 2),
+      new Ingredient('pork', 3)
+    ]),
+    new Recipe('fourth recipe', 'This is simply a test', 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQRpGdr_SnetaYnH4mKII5dqx9lDI6_juo2QPl0rvztlSlfIUg', [
+      new Ingredient('Yams', 1),
+      new Ingredient('paw paw', 2),
+      new Ingredient('pork', 3)
+    ]),
+    new Recipe('fifth recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/d/df/Dahi_baigana_Odia_recipe.jpg', [
+      new Ingredient('Flour', 1),
+      new Ingredient('Grapes', 2),
+      new Ingredient('pork', 3)
+    ]),
   ];
 
   constructor() { }
